cf96700b5e041042ba68267015e962a73e322647
Ege Ersoz <ege.ersoz@gmail.com>
2016-09-19 12:50:35 -0500

    prepare for heroku deployment

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,9 @@ defmodule Zenowiki.UserSocket do
   # channel "room:*", Zenowiki.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  ## Transports
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
