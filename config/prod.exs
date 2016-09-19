2685283561280b5940fd35e85c7b8b47c1f81d09
Ege Ersoz <ege.ersoz@gmail.com>
2016-09-19 12:52:33 -0500

    prepare for heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :zenowiki, Zenowiki.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "zenowiki.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
